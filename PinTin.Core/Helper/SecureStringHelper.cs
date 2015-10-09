ff83624e9509373871da2ce377f9658f1ddd8161
Daniel Gilbert <ich@danielgilbert.de>
2015-10-09 22:40:27 +0200

    Adds SecureStringHelper

---
+++ PinTin.Core/Helper/SecureStringHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinTin.Core.Helper
+{
+    public static class SecureStringHelper
+    {
+        public static SecureString MakeStringSecure(string strPassword)
+        {
+            var secureStr = new SecureString();
+            if (strPassword.Length > 0)
+            {
+                foreach (var c in strPassword.ToCharArray()) secureStr.AppendChar(c);
+            }
+            return secureStr;
+        }
+
+        public static string MakeInsecure(SecureString secstrPassword)
+        {
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(secstrPassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+    }
+}
